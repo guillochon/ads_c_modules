cb28b9cc142dc490c30f193deac73eeb89018f59
Edwin Henneken <ehenneken@gmail.com>
2014-02-05 12:29:31 -0500

    Removed some legacy stuff

--- setup.py
+++ setup.py
@@ -6,19 +6,8 @@ import os
 import sys
 from distutils.core import setup
 from distutils.extension import Extension
-from ads.ads_c_modules.utils import get_platform_specific_module
 
 MODULES = ['Looker', 'ctrigram', 'ldw']
-
-PLATFORM_DIR = 'ads_c_modules/' + get_platform_specific_module()
-
-print sys.argv
-
-if len(sys.argv) > 1 and sys.argv[1] == 'install':
-    print "Installing in " + PLATFORM_DIR
-    if not os.path.exists('../' + PLATFORM_DIR):
-        os.makedirs('../' + PLATFORM_DIR)
-        os.system('touch ../' + PLATFORM_DIR + '/__init__.py')
 
 EXTENSIONS = [Extension(name=mod, sources=[mod + 'module.c'])
         for mod in MODULES]
@@ -30,6 +19,5 @@ setup (
      author       = "Markus Demleitmer",
      author_email = "mdemleit@tucana.harvard.edu",
      url          = "http://adsabs.harvard.edu",
-     extra_path   = 'ads/' + PLATFORM_DIR,
      ext_modules  = EXTENSIONS,
 )
